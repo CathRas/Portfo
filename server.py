5196272f3b043e90475fffec88b331427ce74a17
CathRas <111048573+CathRas@users.noreply.github.com>
2025-07-11 21:14:05 +0200

    New Detail for my Portfolio

---
+++ server.py
@@ -0,0 +1,85 @@
+<<<<<<< HEAD
+from urllib import request
+from flask import Flask, render_template, url_for, request, redirect
+import csv
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data.get("email")
+        subject = data.get("subject")
+        message = data.get("message")
+        file = database.write(f"Email: {email}\nSubject: {subject}\nMessage: {message}\n\n")
+
+def write_to_csv(data):
+    with open('database.csv', mode='a', newline='') as database2:
+        email = data.get("email")
+        subject = data.get("subject")
+        message = data.get("message")
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return "There was an issue submitting the form"
+    else:
+        return "Please submit the form"
+=======
+from urllib import request
+from flask import Flask, render_template, url_for, request, redirect
+import csv
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data.get("email")
+        subject = data.get("subject")
+        message = data.get("message")
+        file = database.write(f"Email: {email}\nSubject: {subject}\nMessage: {message}\n\n")
+
+def write_to_csv(data):
+    with open('database.csv', mode='a', newline='') as database2:
+        email = data.get("email")
+        subject = data.get("subject")
+        message = data.get("message")
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return "There was an issue submitting the form"
+    else:
+        return "Please submit the form"
+>>>>>>> 210393df1972d9de0c67dc54fecc42b764e232e6
